fe9f02835f1565eb9726b6c9eda0b8eac0cf29ca
Fardeen Afsar <46843508+FardeenAfsar@users.noreply.github.com>
2024-01-23 02:10:45 -0500

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -35,9 +35,9 @@ export default {
         status: false,
         clientId: process.env.VUE_APP_SP_CLIENT_ID,
         clientSecret: process.env.VUE_APP_SP_CLIENT_SECRET,
-        authCode: '',
-        accessToken: '',
-        refreshToken: ''
+        authCode: 'AQAwbQWRc-t8nK2OhvCgjhY0FRfotaNw4GC_XPhgBI8_4jcr4u2_uLzzQcvS3sXGIrFtR6Ia4qXCKxAiQpDttCBfBouXU5mUliL0FN6DyAKtRD4mOH_YrcuRxbCoaUA7rybyllqinDUiVzRCIxo3gzsdEjfAsLbu2nTA9YIp46numtv2_ip4kpYehhd1XruHo8t-JGvw',
+        accessToken: 'BQDk5UHjzApkPeN3lcPqp_7C9N0NHU2hUFI0uxKmQJIfVWh9iFne4x191NH-olJPd6Xc6R_3-xGcz5mxx7VyJay7NpAZVPpSw7AaDrxdP_0Y1g-oi_iKf0svULvyzay6nMr43Hep74NkmDoGTGs4lCBmE4TDX-5dHRoBiUji-LbqYw47h33gc2GKsCAzAWeBAmI0n7ihIRc',
+        refreshToken: 'AQCGFQgdH9Re8YTSztQ1Li0w37zsWBOAZhy1jvdeMMibBmB4mHD2oz1IzC90VCBKe_swwww9HQPRuYxqhFBQ2HVraRkrcll_aZ1WbDG0-5lzZsyv-W_jvYjbXnvAhoyaT7c'
       },
       endpoints: {
         auth: 'https://accounts.spotify.com/authorize',
